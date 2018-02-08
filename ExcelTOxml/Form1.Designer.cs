630c9585af19daaf67ad22fabcc7f9919a3ea769
ZhouYong <zhyong319@163.com>
2018-02-08 22:25:56 +0800

    init soft version1.0,The funcation is a excel file to xml file

---
+++ ExcelTOxml/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace ExcelTOxml
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();          
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(29, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "加载EXCEL文件";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(173, 49);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(225, 32);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("微软雅黑", 25.81132F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.ForeColor = System.Drawing.Color.Maroon;
+            this.button2.Location = new System.Drawing.Point(12, 168);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(489, 64);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "生成XML文件";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button3.Location = new System.Drawing.Point(29, 103);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(136, 33);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "保存至...";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox2.Location = new System.Drawing.Point(173, 103);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(225, 32);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("微软雅黑", 12.22642F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button4.ForeColor = System.Drawing.Color.OrangeRed;
+            this.button4.Location = new System.Drawing.Point(412, 49);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(67, 86);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "帮助说明 ";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(489, 141);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "设置";
+            // 
+        
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::ExcelTOxml.Properties.Resources.tim1g;
+            this.ClientSize = new System.Drawing.Size(513, 244);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Excel文件转XML文件";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        
+    }
+}
+
---
+++ ExcelTOxml/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.IO;
+using System.Xml;
+
+namespace ExcelTOxml
+{
+    public partial class Form1 : Form
+    {
+        string excelFilePath;
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e) //加载EXCEL文件
+        {
+            OpenFileDialog f = new OpenFileDialog();
+            //f.Filter = "Excel files(*.xlsx) | *.xlsx |All files(*.*) | *.*";
+            if (f.ShowDialog() != DialogResult.OK) return;
+            excelFilePath = Path.GetFullPath(f.FileName);
+            this.textBox1.Text = excelFilePath;
+        }
+
+        private void button2_Click(object sender, EventArgs e) //生成XML文件
+        {
+            if (this.textBox1.Text == "")
+            {
+                MessageBox.Show("请先加载需要转换的EXCEL文档！！！");
+                return;
+            }
+            if (this.textBox2.Text == "")
+            {
+                MessageBox.Show("请指定生成的XML文件保存位置！！！");
+                return;
+            }
+
+
+            string connstring = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + excelFilePath + ";Extended Properties='Excel 8.0;HDR=NO;IMEX=1';"; // Office 07及以上版本 不能出现多余的空格 而且分号注意                                                                                                                                            //string connstring = Provider=Microsoft.JET.OLEDB.4.0;Data Source=" + path + ";Extended Properties='Excel 8.0;HDR=NO;IMEX=1';"; //Office 07以下版本 
+            using (OleDbConnection connection = new OleDbConnection(connstring))
+            {
+                connection.Open();
+                DataTable sheetsName = connection.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, new object[] { null, null, null, "Table" }); //得到所有sheet的名字
+                string firstSheetName = sheetsName.Rows[0][2].ToString(); //得到第一个sheet的名字
+                string sqlcommand = string.Format("SELECT * FROM [{0}]", firstSheetName); //查询字符串
+                OleDbCommand command = new OleDbCommand(sqlcommand, connection);               
+                OleDbDataReader excelReader = command.ExecuteReader();
+                string xmlfilePath = CreateNewXmlFile(); //创建XML文件
+
+                while (excelReader.Read()) //读取全部数据
+                {
+                    // MessageBox.Show(excelReader.GetString(0) + ", " + excelReader.GetString(1));
+                    try
+                    {
+                        WriteStrToXMLex(excelReader.GetString(0), excelReader.GetString(1), xmlfilePath);
+                    }
+                    catch (InvalidCastException)
+                    {
+                        MessageBox.Show("转换已中止！\n\n请将EXCEL单元格的数据数据类型更改为[文本]类型");
+                        return;
+                    }
+                }
+
+                // always call Close when done reading.
+                excelReader.Close();
+                MessageBox.Show("恭喜！转换操作完成!","结果...",MessageBoxButtons.OK);
+            }
+        }
+
+        private void WriteStrToXMLex(string section, string name, string fileName)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(fileName);
+            XmlElement root = doc.DocumentElement;
+            XmlElement numberElemnet = doc.CreateElement("USER"); //创建root根下number子节点
+            XmlElement childElemnet_partment = doc.CreateElement("Section");  //创建number子节点下的section节点
+            XmlElement childElement_name = doc.CreateElement("Name");//创建number子节点下的name节点
+            childElemnet_partment.InnerText = section; //设置子节点的文本
+            childElement_name.InnerText = name;//设置子节点的文本
+            numberElemnet.AppendChild(childElemnet_partment); //添加子节点
+            numberElemnet.AppendChild(childElement_name);
+            root.AppendChild(numberElemnet);
+
+            doc.Save(fileName);
+        }
+
+        private void button3_Click(object sender, EventArgs e) //选择XML保存位置
+        {
+            
+             FolderBrowserDialog fd = new FolderBrowserDialog();
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+                this.textBox2.Text = fd.SelectedPath;
+            }
+            
+
+        }
+
+        private string CreateNewXmlFile() //创建新文件 
+        {
+            DateTime dt = DateTime.Now;
+           
+            string _newFilePath;
+            string fileName = "\\UserInfoLib";
+            string extName = ".xml";
+            _newFilePath = this.textBox2.Text + fileName + dt.Hour.ToString() + dt.Minute.ToString() + dt.Second.ToString() + extName;
+            XmlDocument xml = new XmlDocument();
+            XmlDeclaration decl = xml.CreateXmlDeclaration("1.0", "utf-8", null);
+            xml.AppendChild(decl); //创建声明
+            XmlElement rootEle = xml.CreateElement("ROOT");
+            xml.AppendChild(rootEle); //创建根节点
+            xml.Save(_newFilePath);//保存文件
+           return _newFilePath;
+        }
+
+        private void button4_Click(object sender, EventArgs e)  //HELP帮助文档
+        {
+            Form2 f = new Form2();
+            
+            f.ShowDialog();
+            
+        }
+    }
+}
